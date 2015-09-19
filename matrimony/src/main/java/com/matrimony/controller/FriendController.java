57fd6c1f1aeb6b9e539a9fa37e1732dfbb61ad71
Son <sondcgc00681@fpt.edu.vn>
2015-09-19 16:55:06 +0700

    toi uu code

--- matrimony/src/main/java/com/matrimony/controller/FriendController.java
+++ matrimony/src/main/java/com/matrimony/controller/FriendController.java
@@ -5,22 +5,14 @@
  */
 package com.matrimony.controller;
 
-import com.matrimony.database.FriendDAO;
-import com.matrimony.database.UserDAO;
-import com.matrimony.entity.Friend;
-import com.matrimony.entity.User;
-import com.matrimony.exception.STException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.matrimony.database.FriendDAO;
+import com.matrimony.entity.Friend;
 
 /**
  *
